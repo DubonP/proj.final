2838015e4ea3889c29f7d2c49d6025c9334882ba
Kantuta Molina <kantuta23@gmail.com>
2022-04-20 07:49:46 -0600

    produto

---
+++ src/Pages/Details/data.js
@@ -0,0 +1,12 @@
+
+const produto = {
+    id:1,
+    title:"Cafeteira Elétrica Cadence Neo Mint com Copo Térmico",
+    price:100,
+    category:{id:1, name:"fone"},
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    image:"https://www.sony.pt/image/57f06d3e66ee7ef1a085e16c5e45bc40?fmt=png-alpha&wid=440",
+}
+
+
+export default produto;
